0228f30497d78324842d98c8b1364b692035d3a0
equusbambusoideae <zhu2qian1@gmail.com>
2020-09-06 02:51:45 +0900

    Update Constants.py

--- src/Constants.py
+++ src/Constants.py
@@ -1,21 +1,31 @@
+from JsonTools import JsonTools as jt
+import sys
+
+
 class Constants:
     path_heb = r"./src/heb.json"
     path_porl = r"./src/porl.json"
-    lang_porl = {
+    porl_data = {
         "vowels": {
-            "a": ["low", "center", "unrounded"],
-            "e": ["mid", "front", "unrounded"],
-            "i": ["high", "front", "unrounded"],
-            "o": ["mid", "back", "rounded"],
-            "u": ["high", "back", "rounded"],
+            "a": ["low", "center", "unrounded", "short"],
+            "e": ["mid", "front", "unrounded", "short"],
+            "i": ["high", "front", "unrounded", "short"],
+            "o": ["mid", "back", "rounded", "short"],
+            "u": ["high", "back", "rounded", "short"],
         },
         "consonants": {
             "": [None],
             "p": ["voiceless", "bilabial", "plosive"],
             "t": ["voiceless", "dental", "plosive"],
+            "s": ["voiceless", "dental", "fricative"],
             "k": ["voiceless", "velar", "plosive"],
+            "h": ["voiceless", "glottal", "fricative"],
+            "w": ["voiced", "bilabial", "approximant"],
+            "m": ["voiced", "bilabial", "nasal"],
+            "n": ["voiced", "dental", "nasal"],
             "r": ["voiced", "dental", "trill"],
             "l": ["voiced", "dental", "lateral approximant"],
+            "j": ["voiced", "palatal", "approximant"],
         },
         "parameters": {},
         "phonetic_rules": {
@@ -25,3 +35,11 @@ class Constants:
             "max_ccluster": 2,
         },
     }
+
+
+if __name__ == "__main__":
+    try:
+        if sys.argv[1] == "save":
+            jt.save(Constants.path_porl, Constants.porl_data)
+    except IndexError:
+        pass
