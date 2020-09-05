67a69d6fb2ef42af56bda43bddaf12f32dbae5d2
equusbambusoideae <zhu2qian1@gmail.com>
2020-09-06 03:51:38 +0900

    Update pg.py

--- pg.py
+++ pg.py
@@ -1,2 +1,6 @@
 from src.Constants import Constants as cnst
+from src.GenerateSyllable import GenerateSyllable as gs
 from src.JsonTools import JsonTools as jt
+from src.Language import Language as lang
+
+porl = lang(jt.load(src=cnst.porl_path))
