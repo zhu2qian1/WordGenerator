633517d563f3a45b20ea47907369d29521be2ad5
equusbambusoideae <zhu2qian1@gmail.com>
2020-09-06 02:36:33 +0900

    Update pg.py

--- pg.py
+++ pg.py
@@ -1,2 +1,5 @@
-from src import Constants as cnst
-from src import JsonTools as jt
+from src.Constants import Constants as cnst
+from src.JsonTools import JsonTools as jt
+
+p = jt.load(cnst.path_porl)
+eset = {1, 2, 3, "Ni!", "Knight"}
