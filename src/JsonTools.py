3a610ac79ee9a6e5ddfd9796aa459cfb6f2b545e
equusbambusoideae <zhu2qian1@gmail.com>
2020-09-06 04:11:08 +0900

    Update JsonTools.py

--- src/JsonTools.py
+++ src/JsonTools.py
@@ -1,12 +1,20 @@
 class JsonTools:
     @staticmethod
-    def save(dest: str, content: dict):
+    def save(dest: str, obj: object):
+        """saves an object as a json file.
+        refer to json.dump for more information.
+        
+        dest: str - the path where the json file will be saved
+        obj: object - the object to be saved"""
         import json
         with open(dest, "w") as f:
-            json.dump(content, f, indent=4)
+            json.dump(obj, f, indent=4)
 
     @staticmethod
     def load(src: str) -> object:
+        """loads json from given path.
+
+        src: str - path to the source json file"""
         import json
         with open(src, "r") as f:
             return json.load(f)
