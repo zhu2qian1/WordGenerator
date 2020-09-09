233332d72b39dfb287505d4038ed03b6e703b2f9
Amuzak, I <sennuma@gmail.com>
2020-09-09 22:33:18 +0900

    Update GenerateSyllable.py

--- src/GenerateSyllable.py
+++ src/GenerateSyllable.py
@@ -1,6 +1,9 @@
-from src.Language import Language as lang
+try:
+    from Language import Language as lang
+except ModuleNotFoundError:
+    from src.Language import Language as lang
 
 
 class GenerateSyllable:
     def __init__(self, data: dict) -> object:
-        self.__data = data.deepcopy()
+        self.__data = data.copy()
