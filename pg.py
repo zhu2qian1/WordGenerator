1aac74b0698233e3130665c325f5b5e02342293b
Amuzak, I <sennuma@gmail.com>
2020-09-11 22:35:52 +0900

    Update pg.py

--- pg.py
+++ pg.py
@@ -2,3 +2,5 @@ from src.Paths import porl_path
 from src.JsonTools import JsonTools as jt
 from src.Language import Language as lang
 from src.GenerateSyllable import GenerateSyllable as gs
+
+porl = lang(jt.load(porl_path))
